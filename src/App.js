b716eecc34c42d773588aee30cfcbf6499c1232a
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-04-24 15:21:45 -0300

    Inicializar projeto usando o Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Olá, Mundo!
+      </header>
+    </div>
+  );
+}
+
+export default App;
